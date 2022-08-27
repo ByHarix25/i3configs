3ecb9b2321ca0a99d958be165364501a4483c370
ByHarix25 <8374589+ByHarix25@users.noreply.github.com>
2022-08-27 18:33:11 -0400

    main

--- start.py
+++ start.py
@@ -11,7 +11,23 @@ else:
 size = int(os.popen('ls $HOME/.config/bg | wc -l').read())
 bgnum = rd.randint(1, size)
 
-os.system(f'exec feh --no-fehbg --bg-scale $HOME/.config/bg/bg{bgnum}.jpg')
+
+
+try:
+  os.system(f'exec feh --no-fehbg --bg-scale $HOME/.config/bg/bg{bgnum}.jpg')
+except:
+    try:
+        os.system(f'exec feh --no-fehbg --bg-scale $HOME/.config/bg/bg{bgnum}.png')
+    except:
+        print('[ERROR] The selected background has the wrong file format! Please use .png or .jpg\nTrying to find a background')
+    for x in size:
+        try:
+            os.system(f'exec feh --no-fehbg --bg-scale $HOME/.config/bg/bg{bgnum}.jpg')
+        except:
+            try:
+                os.system(f'exec feh --no-fehbg --bg-scale $HOME/.config/bg/bg{bgnum}.png')
+    print('Could not find any background')
+
 if (os.system('ps -e | grep xmobar') == 256):
     os.system('xmobar')
 
